46b9b42aec2dee308404c849cc59528fe0a2b452
CreamelStudio <hlee7393@gmail.com>
2024-11-22 11:10:05 +0900

    게임 완료!!

--- PhotonLearn/Practice01/Assets/Scripts/Player.cs
+++ PhotonLearn/Practice01/Assets/Scripts/Player.cs
@@ -13,6 +13,8 @@ public class Player : MonoBehaviourPunCallbacks
 
     public float maxSpeed;
 
+    public int jumpCount;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -31,15 +33,30 @@ public class Player : MonoBehaviourPunCallbacks
         if (photonView.IsMine && rb.velocity.x < maxSpeed)
         {
 
-            rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, 0));
+            rb.AddForce(new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0));
         }
     }
     [PunRPC]
     public void Jump()
     {
-        if (photonView.IsMine && Input.GetKeyDown(KeyCode.Space))
+        if (photonView.IsMine && Input.GetKeyDown(KeyCode.Space) && jumpCount !> 0)
         {
             rb.AddForce(Vector3.up * jumpPower, ForceMode2D.Impulse);
+            jumpCount--;
+        }
+    }
+
+    [PunRPC]
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            jumpCount = 2;
+        }
+        if (collision.gameObject.CompareTag("Dead"))
+        {
+            transform.position = new Vector3(0, 0, 0);
         }
     }
 }
--- PhotonLearn/Practice01/Assets/Scripts/PlayerManager.cs
+++ PhotonLearn/Practice01/Assets/Scripts/PlayerManager.cs
@@ -6,8 +6,20 @@ using Photon.Realtime;
 using ExitGames.Client.Photon;
 public class PlayerManager : MonoBehaviourPunCallbacks
 {
+    public Vector3 offset;
 
     private void Start() {
         PhotonNetwork.Instantiate("playerPrefab", new Vector3(0, 0, 0), Quaternion.identity, 0);
+    }
+
+    private void Update()
+    {
+        for(int i = 0; i < FindObjectsOfType<PhotonView>().Length; i++)
+        {
+            if (FindObjectsOfType<PhotonView>()[i].IsMine)
+            {
+                if(FindObjectsOfType<PhotonView>()[i].gameObject.name != "NetworkManager") Camera.main.transform.position = FindObjectsOfType<PhotonView>()[i].gameObject.transform.position + offset;
+            }
+        }
     }
 }
